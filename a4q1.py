52e8709be54378743029a7d49d81d5113d1fc0d7
Aman Riat <amanriat777@gmail.com>
2018-11-16 15:25:06 -0500

    Finished Add_V2, left Add_V1 and main for you

--- a4q1.py
+++ a4q1.py
@@ -1,4 +1,5 @@
 def add(x):
+    '''
     d = -1
     for y in range(len(x)):
         d += 1
@@ -6,3 +7,21 @@ def add(x):
             z += 1
             print (z)
     return x
+    '''
+
+
+def add_V2(y):
+    new = y.copy()
+    a = []
+    z = (len(y))
+    for d in range(0,z):
+        b = [a + 1 for a in new[d]]
+        a.append(b)
+    return a
+        
+z = [[2, 3, 4], [5, 6, 7],[8,9,10]]
+print (add_V2(z))
+
+#Main
+
+
